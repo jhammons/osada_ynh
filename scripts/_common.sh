d2bd1d7bcca248e3251f181e7bbf1528a03fd35c
anmol <github@datamol.org>
2019-04-02 00:18:20 +0530

    php version 7.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-# ============= FUTURE YUNOHOST HELPER =============
-# Delete a file checksum from the app settings
+# =============================================================================
+#                     YUNOHOST 2.7 FORTHCOMING HELPERS
+# =============================================================================
+
+# Create a dedicated php-fpm config
 #
-# $app should be defined when calling this helper
-#
-# usage: ynh_remove_file_checksum file
-# | arg: file - The file for which the checksum will be deleted
-ynh_delete_file_checksum () {
-	local checksum_setting_name=checksum_${1//[\/ ]/_}	# Replace all '/' and ' ' by '_'
-	ynh_app_setting_delete $app $checksum_setting_name
+# usage: ynh_add_fpm_config
+ynh_add_fpm7.1_config () {
+	# Configure PHP-FPM 7.1 by default
+	local fpm_config_dir="/etc/php/7.1/fpm"
+	local fpm_service="php7.1-fpm"
+	ynh_app_setting_set $app fpm_config_dir "$fpm_config_dir"
+	ynh_app_setting_set $app fpm_service "$fpm_service"
+	finalphpconf="$fpm_config_dir/pool.d/$app.conf"
+	ynh_backup_if_checksum_is_different "$finalphpconf"
+	sudo cp ../conf/php-fpm.conf "$finalphpconf"
+	ynh_replace_string "__NAMETOCHANGE__" "$app" "$finalphpconf"
+	ynh_replace_string "__FINALPATH__" "$final_path" "$finalphpconf"
+	ynh_replace_string "__USER__" "$app" "$finalphpconf"
+	sudo chown root: "$finalphpconf"
+	ynh_store_file_checksum "$finalphpconf"
+
+	if [ -e "../conf/php-fpm.ini" ]
+	then
+		finalphpini="$fpm_config_dir/conf.d/20-$app.ini"
+		ynh_backup_if_checksum_is_different "$finalphpini"
+		sudo cp ../conf/php-fpm.ini "$finalphpini"
+		sudo chown root: "$finalphpini"
+		ynh_store_file_checksum "$finalphpini"
+	fi
+	sudo systemctl reload $fpm_service
 }
+
+# Remove the dedicated php-fpm config
+#
+# usage: ynh_remove_fpm7.1_config
+ynh_remove_fpm7.1_config () {
+	local fpm_config_dir=$(ynh_app_setting_get $app fpm_config_dir)
+	local fpm_service=$(ynh_app_setting_get $app fpm_service)
+	ynh_secure_remove "$fpm_config_dir/pool.d/$app.conf"
+	ynh_secure_remove "$fpm_config_dir/conf.d/20-$app.ini" 2>&1
+	sudo systemctl reload $fpm_service
+}
+
+
